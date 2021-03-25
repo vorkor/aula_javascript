9a75d0b285c523a80320a269c6fac1a173032382
vorkor <g_galante@hotmail.com>
2021-03-25 15:22:51 -0300

    Fim da aula 2

--- js/main.js
+++ js/main.js
@@ -1,8 +1,21 @@
 var nome = "Guilherme Galante";
 var idade = 42;
 var idade2 = 10;
+var lista = ["maça" , "pera" , "banana"];
+var fruta = {nome: "maça", cor: "vermelha" };
+lista.push("uva");
+
+//lista.pop(); 
 
 //alert("nome: " + nome +" idade: " + idade);
 
-console.log(nome);
-console.log(idade);
+//console.log(nome);
+//console.log(idade);
+//console.log(lista);
+//console.log(lista.length);
+//console.log(lista.reverse());
+//console.log(lista.toString());
+//console.log(lista.join(" - "));
+/*console.log(fruta);
+alert(fruta.nome);
+*/
