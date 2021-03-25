92bf84a3422cf82f9096360977dc8eb33dd71b17
vorkor <g_galante@hotmail.com>
2021-03-25 15:49:29 -0300

    Fim por hoje

--- js/main.js
+++ js/main.js
@@ -1,9 +1,15 @@
 var nome = "Guilherme Galante";
 var idade = 42;
 var idade2 = 10;
-var lista = ["maça" , "pera" , "banana"];
-var fruta = {nome: "maça", cor: "vermelha" };
+var lista = ["maça" , "pera" , "banana"]; 
+var fruta = {nome: "maça", cor: "vermelha" }; // dicionario
 lista.push("uva");
+//var idade3 = prompt("Qual sua idade?")
+var count = 0;
+var i = 0;
+var d = new Date();
+
+alert (d.getMonth()+1);
 
 //lista.pop(); 
 
@@ -18,4 +24,23 @@ lista.push("uva");
 //console.log(lista.join(" - "));
 /*console.log(fruta);
 alert(fruta.nome);
+
+
+if(idade3 > 18){
+    alert("maior menor de idade");
+}else{
+    alert("menor de idade");
+}
+*/
+/* repetição do while
+while (count < 5){
+    console.log(i)
+    count++;
+}
+*/
+// repetição for
+/*
+for(count = 0; count <= 5; count++){
+    alert(count);
+};
 */
